02559cdab54889fdb380582ca6f4de6f8bbe07ac
meir <meir.belaich@epitech.eu>
2020-04-27 01:45:40 +0200

    dimanche

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -3,17 +3,23 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class LuckyController extends AbstractController
+class LuckyController extends  AbstractController
 {
     /**
-     * @Route("/lucky", name="lucky")
+     * @Route("/lucky/number")
      */
-    public function index()
+
+    public function show()
     {
-        return $this->render('lucky/index.html.twig', [
-            'controller_name' => 'LuckyController',
-        ]);
+        $meir = 19;
+            return $this->render('lucky/number.html.twig',['meir' => $meir,]);
+    }
+    public function number()
+    {
+        $number = random_int(0, 100);
+        return $this->render('lucky/number.html.twig',['number' => $number,]);
     }
 }
